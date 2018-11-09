39b4d5633d027aeb97b7ba5090333c2d9f144564
TerrySu <2684759855@qq.com>
2018-11-09 09:39:31 +0800

    feat: catpure matching error

--- src/build.ts
+++ src/build.ts
@@ -4,14 +4,22 @@ import * as PATH from "path"
 import { MATCH, INSERT } from "./constant/type"
 
 function matchText( start: string, end: string, sourceText: string ) {
-  let res = ''
+  let res = ""
 
   try {
     const startIndex = sourceText.indexOf( start )
+
+    if ( startIndex === -1 ) {
+      throw `${start} wasn't matched!`
+    }
+
+    if ( sourceText.indexOf( end ) === -1 ) {
+      throw `${end} wasn't matched!`
+    }
+
     const endIndex = sourceText.indexOf( end ) + end.length
     res = sourceText.substring( startIndex, endIndex )
   } finally {
-
   }
 
   return res
@@ -20,20 +28,30 @@ function matchText( start: string, end: string, sourceText: string ) {
 export default function( input: string, outputDir: string, rules: Rule[] ) {
   const sourceText = FS.readFileSync( input, { encoding: "utf8" } )
   function resolveRule( rule: Rule ) {
-    const { start, end, file, before = '', after = '', type = MATCH, inserted = '' } = rule
+    const {
+      start,
+      end,
+      file,
+      before = "",
+      after = "",
+      type = MATCH,
+      inserted = ""
+    } = rule
     let outputingText
     if ( type === MATCH ) {
       const matchedText = matchText( start, end, sourceText )
-      outputingText = `${ before }${ matchedText }${ after }`
+      outputingText = `${before}${matchedText}${after}`
     }
     if ( type === INSERT ) {
-      outputingText = `${ before }${ inserted }${ after }`
+      outputingText = `${before}${inserted}${after}`
     }
 
     const outputPath = PATH.resolve( outputDir, file )
     FS.outputFileSync( outputPath, outputingText )
   }
 
-  const filteredRules = rules.filter( rule => rule.file != null && rule.file.length > 0 )
+  const filteredRules = rules.filter(
+    rule => rule.file != null && rule.file.length > 0
+  )
   filteredRules.forEach( resolveRule )
 }
